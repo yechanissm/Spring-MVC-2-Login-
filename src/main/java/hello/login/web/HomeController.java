8f27f64c4c5d22c5a3a2bc41b11425c86530f678
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-02-08 14:58:38 +0900

    로그인 처리하기 - 서블릿 HTTP 세션1

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Slf4j
@@ -41,7 +42,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model)  {
 
         //세션 관리자에 저장된 회원 정보 조회
@@ -52,6 +53,24 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model)  {
+
+        HttpSession session = request.getSession(false);
+        if(session != null) {
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if(loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
